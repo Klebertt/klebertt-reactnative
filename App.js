cea8fc90580d49cf1d9533c60d933aa54e87125a
Klebertt <jhonattan.ramon.17@gmail.com>
2023-08-02 15:53:52 -0300

    pog

--- App.js
+++ App.js
@@ -2,10 +2,22 @@ import {Text, View} from 'react-native'
 
 const MyApp = () => {
   return (
+    <>
   <View>
     <Text>ABC</Text> 
     <Text>A</Text>
-    </View> )
+    </View> 
+    <Gato/>
+    </>
+    )
+}
+
+
+
+const Gato = () => {
+  return (
+    <Text>GATOGATO</Text>
+  )
 }
 
 export default MyApp
