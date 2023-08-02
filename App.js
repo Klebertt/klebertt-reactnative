ded421cdeaa29a3e2bac33e3293f51098bbf9566
Klebertt <jhonattan.ramon.17@gmail.com>
2023-08-02 15:40:11 -0300

    wow wow

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View} from 'react-native'
 
-export default function App() {
+const MyApp = () => {
   return (
-    <View style={styles.container}>
-      <Text>Funcionando mais beleza!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  <View>
+    <Text>ABC</Text> 
+    <Text>A</Text>
+    </View> )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-let a = 20
+export default MyApp
