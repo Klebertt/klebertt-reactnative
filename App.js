8d5bc0be9fdf0e2a79a7441660db702154252f02
Klebertt <jhonattan.ramon.17@gmail.com>
2023-08-02 16:33:29 -0300

    a

--- App.js
+++ App.js
@@ -9,16 +9,21 @@ const MyApp = () => {
     </View> 
     <Gato nome = "Sparks" idade = {2}/>
     <Gato nome = "Garfield" idade={4}/>
-    <Gato nome = "Marie" idade = {7}/>
+    <Gato nome = "Marie" idade = {1}/>
     </>
     )
 }
 
 
 const Gato = ({nome, idade}) => {
-  return (
-    <Text>Meu nome é {nome.toUpperCase()} e eu tenho {idade} anos.</Text>
+  
+   
+    
+    return (
+    <Text>Meu nome é {nome.toUpperCase()} e eu tenho {idade} {idade === 1 ? 'ano':'anos'}</Text>
   )
 }
+
+
 
 export default MyApp
