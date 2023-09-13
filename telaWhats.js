4d76a130bab2b7a82b8e0e01301f43ddb643f98a
Klebertt <75376776+roberto06rhz@users.noreply.github.com>
2023-09-13 16:30:43 -0300

    o

--- telaWhats.js
+++ telaWhats.js
@@ -3,20 +3,49 @@ import {StyleSheet, View, Text} from 'react-native';
 
 
 
+
+
 const Flex = () => {
   return (
     <View
       style={[
         styles.container,
         {
-          flexDirection: 'column',
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: "column",
         },
-      ]}>
-      <View style={{flex: 0.045, backgroundColor: '#054D45'}} />
-      <View style={{flex: 0.16, backgroundColor: '#065E55'}} />
-      <View style={{flex: 1, backgroundColor: 'white'}} />
-      
+      ]}
+    >
 
+     <View
+        style={{
+          flex: 0.045,
+          backgroundColor: "#054D45",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "flex-end",
+        }}
+      >
+        <Text style={{ color: "white" }}></Text>
+        <Text style={{ color: "white" }}></Text>
+        <Text style={{ color: "white" }}></Text>
+      </View>
+      
+      <View
+        style={{
+          flex: 0.16,
+          backgroundColor: "#065E55",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "flex-end",
+        }}
+      >
+        <Text style={{ color: "white" }}>Conversas</Text>
+        <Text style={{ color: "white" }}>Status</Text>
+        <Text style={{ color: "white" }}>Chamadas</Text>
+      </View>
+
+      <View style={{ flex: 1, backgroundColor: "white" }} />
     </View>
   );
 };
@@ -28,4 +57,39 @@ const styles = StyleSheet.create({
   },
 });
 
+const linhacima = StyleSheet.create({
+  flex: 0.045,
+          backgroundColor: "#054D45",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "flex-end",
+  },
+);
+
 export default Flex;
+
+
+/*const Flex = () => {
+  return (
+    <View
+    
+      style={[
+        styles.container,
+        {
+          flexDirection: 'column',
+        },
+      ]}>
+      <View style={{flex: 0.045, backgroundColor: '#054D45'}}/> 
+      <View style={{flex: 0.16, backgroundColor: '#065E55'}} />
+      <View style={{flex: 1, backgroundColor: 'white'}} />
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+})*/
